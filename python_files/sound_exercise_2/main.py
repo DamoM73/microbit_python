70701b397f721f0cbcbdd3a922a0da75725efdb5
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2023-08-15 09:09:19 +1000

    Adjusted sound

---
+++ python_files/sound_exercise_2/main.py
@@ -0,0 +1,7 @@
+# sound exercise 2
+from microbit import *
+import speech
+
+lyrics = "WIY AAR THIY IY5GUL AHV MAOREHTUN BEY5 BOYZ, WIY SOW4R TAAGEH5DHER TUX RIY5CH NUW /HEY4TS"
+
+phonemes = speech.pronounce(lyrics)
