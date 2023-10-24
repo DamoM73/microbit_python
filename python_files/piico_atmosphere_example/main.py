50fe0764236cabfbf975d99e3f9acdefe638f709
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2023-10-24 13:57:28 +1000

    stuff

--- python_files/piico_atmosphere_example/main.py
+++ python_files/piico_atmosphere_example/main.py
@@ -6,15 +6,15 @@
 from PiicoDev_BME280 import PiicoDev_BME280
 from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
 
+def wbt(temp,hum):
+
+    return (temp - ((1-hum)/5))
+
 sensor = PiicoDev_BME280() # initialise the sensor
-zeroAlt = sensor.altitude() # take an initial altitude reading
 
 while True:
     # Print data
-    tempC, presPa, humRH = sensor.values() # read all data from the sensor
-    pres_hPa = presPa / 100 # convert air pressurr Pascals -> hPa (or mbar, if you prefer)
-    print(str(tempC)+" °C  " + str(pres_hPa)+" hPa  " + str(humRH)+" %RH")
-    
-    # Altitude demo
-#     print(sensor.altitude() - zeroAlt) # Print the pressure CHANGE since the script began
-    sleep_ms(100)
+    tempC, press, humRH = sensor.values() # read all data from the sensor
+    print("Here")
+    #print(wbt(tempC, humRH))
+    sleep_ms(1000)
