d925ea68bb557be258c058fb94ecaeef35c9a41c
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2024-08-23 11:52:00 +1000

    Fixed piico atmos example code

--- python_files/piico_atmosphere_example/main.py
+++ python_files/piico_atmosphere_example/main.py
@@ -6,15 +6,10 @@
 from PiicoDev_BME280 import PiicoDev_BME280
 from PiicoDev_Unified import sleep_ms # cross-platform compatible sleep function
 
-def wbt(temp,hum):
-
-    return (temp - ((1-hum)/5))
-
 sensor = PiicoDev_BME280() # initialise the sensor
 
 while True:
     # Print data
     tempC, press, humRH = sensor.values() # read all data from the sensor
-    print("Here")
-    #print(wbt(tempC, humRH))
+    print("Temp: " + str(tempC) + "\tHumidity: " + str(humRH) + "\tAir presure: " + str(press))
     sleep_ms(1000)
